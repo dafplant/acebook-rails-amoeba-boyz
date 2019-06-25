8d0fe7fe3a2b9fbb59e6c7e57b0a33b61290a8fa
Ben Reynolds-Carr <43816668+breycarr@users.noreply.github.com>
2019-06-25 14:04:26 +0100

    Revert "Add: sign in requirement"

--- spec/features/user_can_sign_out_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Sign out", type: :feature do
-  scenario "User can sign oout" do
-    signup
-    click_link "Sign out"
-    expect(page).to have_content("Signed out successfully.")
-  end
-end
